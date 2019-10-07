7b53d22d6adbcd8338c71c66edf49d46407364c2
sumin_Kim_1202 <pizz201207@naver.com>
2019-10-07 19:10:13 +0900

    캐릭터구현

--- Lecture07_Linear_Movement/line.py
+++ Lecture07_Linear_Movement/line.py
@@ -20,22 +20,25 @@ def handle_events():
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image("KPU_GROUND.png")
-character = load_image('hand_arrow.png')
+mouse = load_image('hand_arrow.png')
+character = load_image('animation_sheet.png')
 
 running = True
 x = 800 // 2
 y = 800 // 2
 frame = 0
 dir = 0
+px, py = 300, 300
 
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    character.clip_draw(frame * 100, 0, 100, 100, x, y)
+    mouse.clip_draw(frame * 100, 0, 100, 100, x, y)
+    character.clip_draw(frame * 100, 0, 100, 100, px, py)
     update_canvas()
     handle_events()
     frame = (frame + 1) & 8
     x += dir * 5
-    delay(0.05)
+    delay(0.01)
 
 close_canvas()
