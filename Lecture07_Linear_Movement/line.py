89bc870eb2b43306df496ed8e873e0c4577b89fc
sumin_Kim_1202 <pizz201207@naver.com>
2019-10-07 20:04:04 +0900

    2018182008 김수민 Drill-05
    커밋횟수 4회

--- Lecture07_Linear_Movement/line.py
+++ Lecture07_Linear_Movement/line.py
@@ -7,17 +7,17 @@ def handle_events():
     # fill here
     global running
     global x, y
+    global remx, remy
+    global px, py
+    global dir
     events = get_events()
     for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_MOUSEMOTION:
+        if event.type == SDL_MOUSEMOTION:
             x, y = event.x, KPU_HEIGHT -1 - event.y
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            remx, remy = event.x - 25, KPU_HEIGHT - 1 - event.y + 25
 
     pass
-
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image("KPU_GROUND.png")
@@ -25,21 +25,36 @@ mouse = load_image('hand_arrow.png')
 character = load_image('animation_sheet.png')
 
 running = True
-x = 800 // 2
-y = 800 // 2
+x = KPU_WIDTH // 2
+y = KPU_HEIGHT // 2
+px = KPU_WIDTH // 2
+py = KPU_HEIGHT // 2
 frame = 0
-dir = 0
-randx, randy = random.randrange(10, KPU_WIDTH), random.randrange(10, KPU_HEIGHT)
+dir = 3
+remx = px
+remy = py
+hide_cursor()
 
 while running:
+    cx=(remx-px)/30
+    cy=(remy-py)/30
+    px+=cx
+    py+=cy
+
+    if remx-px < 0:
+        dir = 0
+    elif remx-px > 0:
+        dir = 1
+    elif remx-px == 0:
+        dir = 3
+
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    mouse.clip_draw(frame * 100, 0, 100, 100, x, y)
-    character.clip_draw(frame * 100, 0, 100, 100, randx, randy)
+    mouse.draw(x, y)
+    character.clip_draw(frame * 100, 100*dir, 100, 100, px, py)
     update_canvas()
-    handle_events()
     frame = (frame + 1) & 8
-    x += dir * 5
     delay(0.01)
+    handle_events()
 
 close_canvas()
