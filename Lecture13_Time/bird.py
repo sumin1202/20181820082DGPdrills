ebb78a6788fe848c6937cd6d6ae88fa4240665b4
sumin_Kim_1202 <pizz201207@naver.com>
2019-11-12 23:01:18 +0900

    Drill10-완성
    2018182008 김수민
    https://www.youtube.com/watch?v=o0IZCFoR0rg&feature=youtu.be
    커밋횟수 3회

--- Lecture13_Time/bird.py
+++ Lecture13_Time/bird.py
@@ -2,8 +2,8 @@ from pico2d import *
 import game_framework
 import game_world
 
-PIXEL_PER_METER = (10.0 / 0.3)
-RUN_SPEED_KMPH = 60.0
+PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
+RUN_SPEED_KMPH = 60.0   # Km / Hour
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
